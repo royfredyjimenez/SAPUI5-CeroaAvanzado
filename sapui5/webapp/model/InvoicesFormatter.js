832d116936697ad536fb5812d9ac077ee74f0c7f
ROY FREDY JIMENEZ BARRON <royfredyjimenez@gmail.com>
2023-09-25 23:03:23 +0000

    8.5. Filtros

---
+++ sapui5/webapp/model/InvoicesFormatter.js
@@ -0,0 +1,19 @@
+// @ts-ignore
+sap.ui.define([], function () {
+    let oFormatter = {
+
+        invoicesStatus: function (sStatus) {
+        
+            let oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+            switch (sStatus) {
+                case 'A': return oResourceBundle.getText("invoiceStatusA");
+                case 'B': return oResourceBundle.getText("invoiceStatusB");
+                case 'C': return oResourceBundle.getText("invoiceStatusC");
+            }
+        },
+
+    }
+    return oFormatter;
+});
+
